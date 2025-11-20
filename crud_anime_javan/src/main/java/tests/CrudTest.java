671ffe0a8cdcd6c161e2eb435783957ca0e25605
JavanRosario <oliveiraajavan@hotmail.com>
2025-11-19 23:55:13 -0300

    add new options

--- crud_anime_javan/src/main/java/tests/CrudTest.java
+++ crud_anime_javan/src/main/java/tests/CrudTest.java
@@ -1,27 +1,29 @@
 package tests;
 
-import Repository.ProducerRepository;
 import services.ProducerServices;
 
 import java.util.Scanner;
 
 public class CrudTest {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
         int o;
         while (true) {
             menu();
-            o = Integer.parseInt(scanner.nextLine());
+            o = Integer.parseInt(SCANNER.nextLine());
             if (o == 0) break;
-            ProducerServices.makeMenu(o);
+            ProducerServices.choicesMenu(o);
 
         }
     }
 
     private static void menu() {
+        System.out.println("====================================");
         System.out.println("Type the number of your operation");
         System.out.println("1. Search for Producer");
+        System.out.println("2. Delete Producer");
         System.out.println("0. For Exit");
+        System.out.println("====================================");
     }
 }
