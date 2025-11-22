52ad51181263867285989da15b9a256afe1db05b
JavanRosario <oliveiraajavan@hotmail.com>
2025-11-22 20:26:32 -0300

    add string validation

--- crud_anime_java/src/main/java/tests/CrudTest.java
+++ crud_anime_java/src/main/java/tests/CrudTest.java
@@ -12,55 +12,75 @@ public class CrudTest {
         int o;
         while (true) {
             menu();
-            o = Integer.parseInt(SCANNER.nextLine());
+            o = readInt();
+
             if (o == 0) break;
+
             switch (o) {
                 case 1 -> {
                     producerMenu();
-                    o = Integer.parseInt(SCANNER.nextLine());
+                    o = readInt();
                     ProducerServices.choicesMenu(o);
                 }
                 case 2 -> {
                     animeMenu();
-                    o = Integer.parseInt(SCANNER.nextLine());
+                    o = readInt();
                     AnimeServices.choicesMenu(o);
                 }
             }
         }
     }
 
+    public static int readInt() {
+        while (true){
+            String input = SCANNER.nextLine();
+            try {
+                int i = Integer.parseInt(input);
+                return i;
+            }catch (NumberFormatException e){
+                System.out.println("Typing only numbers");
+            }
+        }
+    }
+
     private static void menu() {
         System.out.println("====================================");
-        System.out.println("Type the number of your operation");
-        System.out.println("1. Producer");
-        System.out.println("2. Anime");
+        System.out.println("        MAIN MENU - SYSTEM");
+        System.out.println("====================================");
+        System.out.println("1. Producer Menu");
+        System.out.println("2. Anime Menu");
         System.out.println("0. Exit");
         System.out.println("====================================");
         System.out.print("Choose an option: ");
     }
 
+
     private static void producerMenu() {
         System.out.println("====================================");
-        System.out.println("Type the number of your operation");
-        System.out.println("1. Search for Producer | Show all");
+        System.out.println("          PRODUCER MENU");
+        System.out.println("====================================");
+        System.out.println("1. Search Producer | Show all");
         System.out.println("2. Delete Producer");
         System.out.println("3. Insert New Producer");
         System.out.println("4. Update Producer");
-        System.out.println("9. Go back");
+        System.out.println("9. Go back to Main Menu");
         System.out.println("====================================");
         System.out.print("Choose an option: ");
     }
 
+
     private static void animeMenu() {
         System.out.println("====================================");
-        System.out.println("Type the number of your operation");
-        System.out.println("1. Search for Anime | Show all");
+        System.out.println("            ANIME MENU");
+        System.out.println("====================================");
+        System.out.println("1. Search Anime | Show all");
         System.out.println("2. Delete Anime");
         System.out.println("3. Insert New Anime");
         System.out.println("4. Update Anime");
-        System.out.println("9. Go back");
+        System.out.println("9. Go back to Main Menu");
         System.out.println("====================================");
         System.out.print("Choose an option: ");
     }
+
 
 }
